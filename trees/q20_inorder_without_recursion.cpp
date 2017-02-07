b76111cd73e40ad8d4302eaf45584512a426d3c7
rgupta <communication.rishabh@gmail.com>
2017-02-08 00:14:43 +0530

    Inorder Traversal Iteratively

---
+++ trees/q20_inorder_without_recursion.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include<cstring>
+#include<stack>
+using std::stack;
+/* structure of the tree */
+typedef struct node {
+	int data;
+	struct node *left;
+	struct node *right;
+}node;
+
+typedef node *tree;
+void inorderTraversal(tree root) {
+	if(!root) {
+		return ;
+  }
+	stack<tree> s;
+	while(true) {
+		while(root != NULL)	{
+			s.push(root);
+			root = root->left;
+		}
+		if(s.empty())	{
+			break;
+		}
+		cout << s.top()->data << ' ';
+		root = s.top();
+		s.pop();
+		root = root->right;
+  } 
+	cout << endl;
+}
+
+void createBinaryTree(tree &root, int data) {
+	tree temp = new node;
+	temp->left = NULL;
+	temp->data = data;
+	temp->right = NULL;
+
+	if(!root) {	
+		root = temp;
+		return ;
+  }
+	char ch;
+	cout << "Press l to insert in left and otherwise for right:";
+	cin >> ch;
+  if(ch == 'l') { 
+		createBinaryTree(root->left, data);
+  } else {
+		createBinaryTree(root->right, data);
+  }
+}
+
+int main() {
+	tree root = NULL;
+	int data;
+	char ch;
+	cout << "Press y to fill data: ";
+	cin >> ch;
+  while(tolower(ch) == 'y') {
+		cout << "Enter data: ";
+		cin >> data;
+    createBinaryTree(root, data);
+		cout << "Press y to fill more data: ";
+	  cin >> ch;
+  }
+	inorderTraversal(root);
+  cout << endl;
+	return 0;  
+}
+
