5b4662d0a1e3e9d99b709906563e3badac571065
rgupta <communication.rishabh@gmail.com>
2017-02-23 21:03:09 +0530

    Implement queue using stack

--- stacks/q01.implement_queue_with_stack.cpp
+++ stacks/q01.implement_queue_with_stack.cpp
@@ -5,7 +5,9 @@ using std::endl;
 #include<stack>
 using std::stack;
 #include<climits>
-
+#include<string>
+using std::string;
+#include<typeinfo>
 template <class T>
 class Queue {
 	private:
@@ -32,11 +34,14 @@ int Queue<T>::size() {
 
 template <class T>
 T Queue<T>::back() {
+	if(s1.empty()) {
+		return (T)INT_MIN;
+  }
 	while(!s1.empty()) {
 		s2.push(s1.top());
 		s1.pop();
   }
-	int data = s2.top();
+	T data = s2.top();
 	while(!s2.empty()) {
 		s1.push(s2.top());
 		s2.pop();
@@ -47,11 +52,11 @@ T Queue<T>::back() {
 template <class T>
 T Queue<T>::front() {
 	if(s1.empty()) {
-		return INT_MIN;
+		return (T)INT_MIN;
   }
+	cout << "inside front\n";
 	return s1.top();
 }
-
 template <class T>
 void Queue<T>::push(const T key) {
 	while(!s1.empty()) {	
@@ -67,6 +72,9 @@ void Queue<T>::push(const T key) {
 
 template <class T>
 T Queue<T>::pop() {
+	if(s1.empty()) {
+		return (T)INT_MIN;
+  }
 	T data = s1.top();
 	s1.pop();
 	return data;
@@ -90,6 +98,9 @@ int main() {
 	cout << "Queue Rear: " << Q.back() << endl;
 	cout << "Popping " << Q.pop() << endl;
 	cout << Q.empty() << endl;
+	cout << "Queue size: " << Q.size() << endl;
+	cout << "Queue front: " << Q.front() << endl;
+	cout << "Queue Rear: " << Q.back() << endl;
 	return 0;
 }
 
